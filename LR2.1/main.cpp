971755b7b36e0344378c46be3e7c59e49bd3337f
Nazar <derkevich.n@gmail.com>
2024-09-15 15:51:41 +0300

    Add class 'HumanGift'

--- LR2.1/main.cpp
+++ LR2.1/main.cpp
@@ -7,13 +7,6 @@ struct Date {
 	int month;
 };
 
-class SomeClass {
-	int inte;
-	float flt;
-	float flt2;
-};
-
-
 class ForEvent {
 protected:
 	std::string type;
@@ -38,6 +31,22 @@ protected:
 public:
 	Item() : name("unknown"), price(0.f) {}
 	Item(std::string n, float p) : name(n), price(p) {}
+
+	virtual void set_name(std::string n) = 0;
+	virtual void set_price(float p) = 0;
+
+	virtual std::string get_name() const = 0;
+	virtual float get_price() const = 0;
+};
+
+class Human {
+protected:
+	std::string name;
+	float price;
+
+public:
+	Human() : name("unknown"), price(0.f) {}
+	Human(std::string n, float p) : name(n), price(p) {}
 
 	virtual void set_name(std::string n) = 0;
 	virtual void set_price(float p) = 0;
@@ -73,6 +82,65 @@ public:
 	~Gift() = default;
 };
 
+class GiftHuman : public Human, public ForEvent {
+private:
+
+public:
+	GiftHuman() : Human(), ForEvent() {}
+	GiftHuman(std::string gift_name, float gift_price, std::string event_type, Date event_date)
+		: Human(gift_name, gift_price), ForEvent(event_type, event_date) {}
+	GiftHuman(std::string gift_name, float gift_price, std::string event_type)
+		: Human(gift_name, gift_price), ForEvent() {}
+
+	virtual void set_name(std::string gift_name) { name = gift_name; }
+	virtual void set_price(float gift_price) { price = gift_price; }
+	virtual void set_type(std::string gift_type) { type = gift_type; }
+	virtual void set_date(Date d) { date = d; }
+
+	virtual Date get_date() const { return date; }
+	virtual std::string get_name()const { return name; }
+	float get_price()const { return price; }
+	virtual std::string get_type()const { return type; }
+
+
+	friend std::ostream& operator<<(std::ostream& s, Gift gift);
+	friend std::ifstream& operator>>(std::ifstream& s, Gift& gift);
+
+	~GiftHuman() = default;
+};
+
+std::ostream& operator<<(std::ostream& s, GiftHuman gift) {
+	s << gift.get_name() << "\t" << gift.get_price() << "\t" << gift.get_type() << "\t" << std::setw(2) << std::setfill('0') << gift.get_date().day << "/" << std::setw(2) << std::setfill('0') << gift.get_date().month;
+	return s;
+}
+
+std::ifstream& operator>>(std::ifstream& s, GiftHuman& gift) {
+	std::string name;
+	float price;
+	std::string type;
+	Date event_date;
+
+	std::string temp, temp2;
+	if (s >> name >> price >> type >> temp)
+	{
+		temp2.assign(temp, 0, 2);
+		event_date.day = std::stoi(temp2);
+		temp2.assign(temp, 3, 5);
+		event_date.month = std::stoi(temp2);
+
+		gift.set_name(name);
+		gift.set_price(price);
+		gift.set_type(type);
+		gift.set_date(event_date);
+	}
+	else
+	{
+		std::cerr << "Empty file";
+	}
+
+	return s;
+}
+
 std::ostream& operator<<(std::ostream& s, Gift gift) {
 	s << gift.get_name() << "\t" << gift.get_price() << "\t" << gift.get_type() << "\t" <<std::setw(2)<<std::setfill('0') << gift.get_date().day << "/" << std::setw(2) << std::setfill('0') << gift.get_date().month;
 	return s;
@@ -104,6 +172,8 @@ std::ifstream& operator>>(std::ifstream& s, Gift& gift) {
 	
 	return s;
 }
+
+
 class Menu {
 private:
 	MyCont<std::string, Gift> container;
